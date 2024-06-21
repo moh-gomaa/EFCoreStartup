f8470e259eb02bd36dcbeee399a971af8f1dc34d
Mohamed Gomaa <mohamed.adel36@msa.edu.eg>
2024-06-21 09:14:35 +0300

    -Make custom migrate to populate static data into database.

---
+++ EFCore/Migrations/20240621061320_PopulateData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO Employees (Name) VALUES ('Predefined Employee 2');
+                                   INSERT INTO Employees (Name) VALUES ('Predefined Employee 3');
+                                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Employees WHERE NAME = 'Predefined Employee 2' OR Name = 'Predefined Employee 3';");
+        }
+    }
+}
