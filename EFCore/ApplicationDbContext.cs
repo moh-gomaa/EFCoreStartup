bb4eca49374d1aafd6515fb9adc95cbb15d95bef
Mohamed Gomaa <mohamed.adel36@msa.edu.eg>
2024-06-21 07:16:44 +0300

    -Add needed nuget packages for EF. -Add ApplicationDbContext class which link database with EF domain models. -Add Employee class which represent Employees table in database. -Insert dummy records in Employees table just for testing.

---
+++ EFCore/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=.;Database=EFCore;Trusted_Connection=True;TrustServerCertificate=True");
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- EFCore/Program.cs
+++ EFCore/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFCore;
+
+var _context = new ApplicationDbContext();
+
+//var employee = new Employee
+//{
+//    Name = "Employee 1"
+//};
+
+var employee = new Employee();
+
+_context.Employees.Add(employee);
+_context.SaveChanges();
